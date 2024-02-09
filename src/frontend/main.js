ffe683f35da851d05e2683cdbdfabf3d8f37dcc9
renan-feitosa <123964834+renan-feitosa@users.noreply.github.com>
2024-02-09 20:12:16 -0300

    feat: Add HTML/JS
    ChangeLog:
    
    - Add src/frontend/
    - Add HTML inputs
    - Add JavaScript logic
    - Add HTML and JS integration

---
+++ src/frontend/main.js
@@ -0,0 +1,31 @@
+var selected_operator = null;
+
+function calculate(){
+    var first_number = document.getElementById('first-number').value
+    var second_number = document.getElementById('second-number').value
+    let result
+
+    switch (selected_operator) {
+        case '+':
+            result = first_number + second_number;
+            break;
+        case '-':
+            result = first_number - second_number;
+            break;
+        case '*':
+            result = first_number * second_number;
+            break;
+        case '/':
+            result = first_number / second_number;
+            break;
+        default:
+            result = 'Selecione um operador válido';
+    }
+    
+    document.getElementById('result').innerText = result
+}
+
+function select_operator(_operator){
+    selected_operator = _operator;
+    console.log(selected_operator)
+}
