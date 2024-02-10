bdbffe1c662b6bdaeb1200371ae27982685d5df6
renan-feitosa <123964834+renan-feitosa@users.noreply.github.com>
2024-02-09 21:38:31 -0300

    feat: Create server
    ChangeLog
    
    - Install express

---
+++ src/backend/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const { Pool } = require('pg');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '..', 'frontend')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}/`);
+});
+
+
